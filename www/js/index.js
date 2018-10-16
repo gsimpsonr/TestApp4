250afcdafed57e4f4bf35d2103647674dcfef1f6
gsimpsonr <43577434+gsimpsonr@users.noreply.github.com>
2018-10-16 15:31:23 +0100

    save chnages

---
+++ www/js/index.js
@@ -0,0 +1,14 @@
+document.addEventListener("deviceready", onDeviceReady, false);
+		
+	
+
+// device APIs are available
+//
+function onDeviceReady() {
+	
+	
+	alert("device ready");
+    
+    
+}
+
